d9eaf65887d53c70bfd344c368d59548c10a38f3
Amir mohammad <am.sanei79@gmail.com>
2020-12-16 20:37:33 +0330

    add List class interface to the project

---
+++ include/List.h
@@ -0,0 +1,28 @@
+#ifndef LIST_H_INCLUDED
+#define LIST_H_INCLUDED
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class List{
+private:
+    typedef struct node{
+        int hour;
+        string pName; // Patient Name
+        string nCode; // National Code
+        node* next;
+    }* nodePtr;
+
+    nodePtr head;
+    nodePtr curr;
+    nodePtr temp;
+public:
+    List();
+    void addNode(int hour,string pName,string nCode);
+    void delNode(string nCode);
+    void printList();
+
+};
+
+#endif // LIST_H_INCLUDED
